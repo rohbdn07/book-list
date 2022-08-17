8a8046fb180ab618ca81dac8b778ea33f2065861
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-17 12:37:00 +0300

    seperated css styles,added  description

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -4,6 +4,27 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 
+// styles...
+const boxStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  position: 'relative',
+  justifyContent: 'space-between',
+  fontFamily: 'monospace',
+  color: 'inherit',
+  margin: '10px ',
+  padding: '10px',
+}
+
+const textStyle = {
+  fontFamily: 'monospace',
+  fontWeight: 700,
+  letterSpacing: '.3rem',
+  color: 'inherit',
+  textDecoration: 'none',
+  textTransform: 'uppercase',
+}
+
 /**
  * @description this component is uses as nav bar
  * @returns JSX Elements
@@ -11,33 +32,9 @@ import Container from '@mui/material/Container'
 const NavBar = (): JSX.Element => {
   return (
     <AppBar position='static'>
-      <Container maxWidth='xl'>
-        <Box
-          sx={{
-            display: { xs: 'none', md: 'flex' },
-            alignItems: 'center',
-            position: 'relative',
-            justifyContent: 'space-between',
-            fontFamily: 'monospace',
-            color: 'inherit',
-            margin: '10px ',
-            padding: '10px',
-          }}
-        >
-          <Typography
-            variant='h6'
-            noWrap
-            component='a'
-            href='/'
-            sx={{
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-              textTransform: 'uppercase',
-            }}
-          >
+      <Container fixed>
+        <Box sx={boxStyle}>
+          <Typography variant='h6' noWrap component='a' href='/' sx={textStyle}>
             Book-List
           </Typography>
         </Box>
