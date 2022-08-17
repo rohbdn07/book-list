401106269bf5317c25722ae3f7ca57184fcf14bb
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-17 12:37:58 +0300

    added: components to render

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,27 @@
+import { styled } from '@mui/material'
 import React from 'react'
 
+import InputForm from '../components/form/InputForm'
+import BookItemsList from '../components/lists/BookList'
+
+// styles...
+const HomeContainer = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-around',
+  paddingTop: '20px',
+})
+
+/**
+ *@description home page component render all components located in home page.
+ * @returns JSX.Element components
+ */
 const Home = () => {
   return (
     <>
-      <p>this is home component</p>
+      <HomeContainer>
+        <InputForm />
+        <BookItemsList />
+      </HomeContainer>
     </>
   )
 }
