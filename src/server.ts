a7016e9b5c8dc55b613fd585aaf4c31a5672490e
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-18 23:48:18 +0300

    refactor some code and added logger and error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-require("dotenv").config();
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import mangoose from "mongoose";
-const path = require("path");
+import path from "path";
 
 import router from "./api/routes/index";
 import { config } from "./config/configDB";
@@ -10,51 +11,75 @@ import Logger from "./library/logger";
 
 const app = express();
 
-//connection to Mangodb...
-const dbURI = config.mongo.url;
-mangoose
-    .connect(dbURI)
-    .then(() => Logger.info("CONNECTED To MongoDB"))
-    .catch((error) => {
-        Logger.error("Unable to Connect to MongoDB");
+/** Start server function */
+const startServer = async () => {
+    //connection to Mangodb...
+    const dbURI = config.mongo.url;
+    await mangoose
+        .connect(dbURI)
+        .then(() => Logger.info("CONNECTED To MongoDB"))
+        .catch((error) => {
+            Logger.error("Unable to Connect to MongoDB");
+            Logger.error(error);
+        });
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        /** log the Request */
+        Logger.info(
+            `Incoming -> Method: [${req.method}] - Url: [${req.url}] -IP: [${req.socket.remoteAddress}]`
+        );
+
+        res.on("finish", () => {
+            /** Log  Response */
+            Logger.info(
+                `Incoming -> Method: [${req.method}] -Url: [${req.url}] -IP: [${req.socket.remoteAddress}] -Status: [${res.statusCode}]`
+            );
+        });
+        next();
+    });
+    // parse json request body
+    app.use(express.json());
+    // parse urlencoded request body
+    app.use(express.urlencoded({ extended: true }));
+    // enable cors
+    app.use(cors());
+
+    /** Routes */
+    app.use("/api/book", router);
+
+    /** Health Check */
+    app.get("/ping", (req: Request, res: Response) =>
+        res.status(200).json({ message: "pong" })
+    );
+
+    // Serve static assists if in PRODUCTION
+    if (process.env.NODE_ENV === "production") {
+        app.use(express.static(path.join(__dirname, "../client/build")));
+        app.get("/*", (req, res) => {
+            res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+        });
+    }
+
+    // The `res.redirect()` function sends back an HTTP 302 by default.
+    // When an HTTP client receives a response with status 302, it will send
+    // an HTTP request to the URL in the response, in this case `/`
+    router.get("*", (req: Request, res: Response) => {
+        res.redirect("/api/book/all");
+    });
+
+    /** Error handling */
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        const error = new Error("Current api route doesnot exist");
         Logger.error(error);
+        return res.status(404).json({ message: error.message });
     });
 
-// parse json request body
-app.use(express.json());
-// parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
-// enable cors
-app.use(cors());
+    //listening to LocalHost | PORT
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => Logger.info(`listening on ${PORT}`));
+    Logger.info("Press CTRL-C to stop\n");
+};
+/** START SERVER */
+startServer();
 
-//get data from db
-app.use("/api/book", router);
-
-// Serve static assists if in PRODUCTION
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-    });
-}
-
-// The `res.redirect()` function sends back an HTTP 302 by default.
-// When an HTTP client receives a response with status 302, it will send
-// an HTTP request to the URL in the response, in this case `/`
-router.get("*", (req, res) => {
-    res.redirect("/api/book/all");
-});
-
-/** Error handling */
-app.use((req, res, next) => {
-    const error = new Error("Current api route doesnot exist");
-    Logger.error(error);
-    return res.status(404).json({ message: error.message });
-});
-
-//listening to LocalHost | PORT
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => Logger.info(`listening on ${PORT}`));
-Logger.info("Press CTRL-C to stop\n");
-
-module.exports = app;
+export default app;
