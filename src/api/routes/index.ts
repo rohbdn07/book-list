9e937b78aa65a3a06593aba38e3f08906d502a8a
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-18 17:21:12 +0300

    added:  new routes

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,6 +2,13 @@ import express from "express";
 const router = express.Router();
 import BookController from "../contollers/book.controller";
 
+// GET: all book list
 router.get("/all", BookController.getAllBooks);
+
+// POST: route to create new book
+router.post("/add", BookController.postBookDetails);
+
+// PUT: route to update book by its ID
+router.put("/update/:id", BookController.updateBookDetails);
 
 export default router;
