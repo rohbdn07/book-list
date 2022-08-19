5e578dd8a87fa7855d7e9146adb35c362118fb4d
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-19 16:51:21 +0300

    added: error handling middleware

---
+++ src/api/middlewares/errorHandler.ts
@@ -0,0 +1,29 @@
+/**
+ *        @file errorHandler.ts
+ *     @summary error handler
+ * @description This middleware handle errors checks and return response
+ *   @functions - errorHandler()
+ */
+
+import { NextFunction, Response, Request } from "express";
+import BaseError from "../../errors/customError";
+import Logger from "../../library/logger";
+
+const errorHandler = (
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (error instanceof BaseError) {
+        Logger.error(error.message);
+        return res.json({ errors: error.serializeErrors() });
+    }
+    Logger.error(error.message);
+    res.json({
+        errors: [{ message: `${error.message}` || "Something went wrong!!!" }],
+    });
+    next();
+};
+
+export default errorHandler;
