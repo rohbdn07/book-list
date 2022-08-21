57cf1d4e15016b4c5994b78582103e3bb331057c
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-21 22:28:04 +0300

    updated and added states, functions to perform CURD functionality

--- client/src/components/form/InputForm.tsx
+++ client/src/components/form/InputForm.tsx
@@ -1,9 +1,17 @@
+import React from 'react'
 import styled from '@emotion/styled'
 import { TextareaAutosize, TextField } from '@mui/material'
-import React from 'react'
 
+import axiosService from '../../services/axios'
+import AlertMessage from '../alert/AlertMessage'
 import ButtonGroupEl from '../buttons/ButtonGroup'
 import HeaderEl from '../headers/HeaderEl'
+
+interface FormDataType {
+  title: string
+  author: string
+  description: string
+}
 
 // styles...
 const FormContainer = styled('div')({
@@ -30,13 +38,29 @@ const FieldContainer = styled('div')({
  * @description it shows input form
  * @returns JSX.Element
  */
-const InputForm = () => {
-  const [inputs, setInputs] = React.useState({
+const InputForm = (props: any) => {
+  const { update, setStateChange } = props
+  const idOfBook = update.id
+
+  const [inputs, setInputs] = React.useState<FormDataType>({
     title: '',
     author: '',
     description: '',
   })
+  const [isError, setIsError] = React.useState(false)
+  const [open, setOpen] = React.useState(false)
+  const [responseMessage, setResponseMessage] = React.useState('')
 
+  /** useEffect() to change state to updated form */
+  React.useEffect(() => {
+    setInputs({
+      title: update.title,
+      author: update.author,
+      description: update.description,
+    })
+  }, [update])
+
+  /** onChange handler for input form */
   const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const name = event.target.name
     const value = event.target.value
@@ -44,21 +68,103 @@ const InputForm = () => {
     setInputs((values) => ({ ...values, [name]: value }))
   }
 
-  const handleSubmit = () => {
+  /** call an action either update or create a book depends on axios service */
+  async function updateORcreate(axiosService: any, text: string) {
     const obj = {
+      id: update.id,
       title: inputs.title,
       author: inputs.author,
       description: inputs.description,
     }
-    console.log(obj)
+    try {
+      // call axios api service to update OR create a book
+      const response = await axiosService(obj)
+      if (response.data.status === true) {
+        setStateChange((prevState: boolean) => !prevState)
+        setIsError(false)
+        setInputs({
+          title: '',
+          author: '',
+          description: '',
+        })
+        setOpen(true)
+        setResponseMessage(text)
+        return
+      } else {
+        setStateChange(false)
+        console.log(response)
+        setIsError(true)
+        setOpen(true)
+        return response.data?.errors.forEach((err: any) => setResponseMessage(err.message))
+      }
+    } catch (error) {
+      console.log('Error', error)
+    }
   }
+
+  /** call an api axios service to delete a book */
+  async function deleteTheBook(axiosService: any, id: string, text: string) {
+    try {
+      console.log('the iddddddd', id)
+      const response = await axiosService(id)
+      if (response.data.status === true) {
+        setStateChange((prevState: boolean) => !prevState)
+        setIsError(false)
+        setInputs({
+          title: '',
+          author: '',
+          description: '',
+        })
+        setOpen(true)
+        setResponseMessage(text)
+        return
+      } else {
+        setStateChange(false)
+        console.log(response)
+        setIsError(true)
+        setOpen(true)
+        return response.data?.errors.forEach((err: any) => setResponseMessage(err.message))
+      }
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
+  /** createNewSubmit() to call api axios service in order to create a new book */
+  const createNewSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const text = 'Succefully Created a new book.'
+    await updateORcreate(axiosService.createBook, text)
+  }
+
+  /** updateSubmit() to call api axios service in order to update the book */
+  const updateSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const text = 'Succefully Updated the book.'
+    await updateORcreate(axiosService.updateBook, text)
+  }
+
+  /** deleteSubmit() to call api axios service in order to delete the book */
+  const deleteSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const text = 'Succefully deleted the book.'
+    await deleteTheBook(axiosService.deleteBook, idOfBook, text)
+  }
+
   return (
     <>
       <FormContainer>
+        <AlertMessage
+          setOpen={setOpen}
+          open={open}
+          responseMessage={responseMessage}
+          isError={isError}
+        />
+
         <HeaderEl text={'Book details'} />
         <FieldContainer>
           <TextField
-            id='outlined-basic'
+            id='outlined-required'
             label='Title'
             name='title'
             variant='outlined'
@@ -66,7 +172,7 @@ const InputForm = () => {
             onChange={handleChange}
           />
           <TextField
-            id='outlined-basic'
+            id='outlined-required'
             label='Author'
             name='author'
             variant='outlined'
@@ -74,6 +180,7 @@ const InputForm = () => {
             onChange={handleChange}
           />
           <TextareaAutosize
+            id='outlined-required'
             aria-label='minimum height'
             name='description'
             value={inputs.description}
@@ -87,7 +194,11 @@ const InputForm = () => {
             }}
           />
         </FieldContainer>
-        <ButtonGroupEl submit={handleSubmit} />
+        <ButtonGroupEl
+          createNewSubmit={createNewSubmit}
+          updateSubmit={updateSubmit}
+          deleteSubmit={deleteSubmit}
+        />
       </FormContainer>
     </>
   )
