cf871a6bf5b09cd2d296886fff27e22079c6eff5
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-21 22:31:29 +0300

    updated and added css elements

--- client/src/components/lists/ListItemEl.tsx
+++ client/src/components/lists/ListItemEl.tsx
@@ -3,9 +3,27 @@ import ListItemText from '@mui/material/ListItemText'
 import Typography from '@mui/material/Typography'
 
 type ListItemElProps = {
+  id?: string
   title: string
   author: string
   description: string
+}
+
+/** display items of list*/
+function DisplayItems({ author, description }: Pick<ListItemElProps, 'author' | 'description'>) {
+  return (
+    <>
+      <Typography
+        sx={{ display: 'inline-flex', paddingRight: '10px' }}
+        component='span'
+        variant='body2'
+        color='text.primary'
+      >
+        {author}
+      </Typography>
+      {`${description.substring(0, 20)}...`}
+    </>
+  )
 }
 
 /**
@@ -15,22 +33,19 @@ type ListItemElProps = {
  * @param description
  * @returns JSX.Element list item
  */
-const ListItemEl = ({ title, author, description }: ListItemElProps) => {
+const ListItemEl = ({ id, title, author, description, handleClick }: ListItemElProps | any) => {
+  // console.log('the id is', id)
+  function update() {
+    return handleClick(id, title, author, description)
+  }
   return (
     <>
       <ListItemText
+        onClick={update}
         primary={title}
         secondary={
           <>
-            <Typography
-              sx={{ display: 'inline' }}
-              component='span'
-              variant='body2'
-              color='text.primary'
-            >
-              {author}
-            </Typography>
-            {`${description.substring(0, 20)}...`}
+            <DisplayItems author={author} description={description} />
           </>
         }
       />
