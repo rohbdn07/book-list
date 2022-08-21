2e098a55d1261c3d384c4cd262d76655a7e31413
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-21 22:34:23 +0300

    updated and added api call function and update the states

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,19 @@ import React from 'react'
 
 import InputForm from '../components/form/InputForm'
 import BookItemsList from '../components/lists/BookList'
+import axiosService from '../services/axios'
+
+export interface IBookData {
+  status?: boolean
+  data?: Book[]
+}
+
+export type Book = {
+  _id: string
+  title: string
+  author: string
+  description: string
+}
 
 // styles...
 const HomeContainer = styled('div')({
@@ -16,11 +29,46 @@ const HomeContainer = styled('div')({
  * @returns JSX.Element components
  */
 const Home = () => {
+  const [data, setData] = React.useState<IBookData>()
+  const [stateChange, setStateChange] = React.useState(false)
+  const [update, setUpdate] = React.useState({
+    id: '',
+    title: '',
+    author: '',
+    description: '',
+  })
+
+  /** useEffect to call axios API service */
+  React.useEffect(() => {
+    callApi()
+  }, [stateChange])
+
+  // call axios services to fetch data
+  const callApi = async () => {
+    try {
+      const response = await axiosService.getBookList()
+      const data = await response.data
+      if (data.status === true) {
+        setData(data)
+      }
+    } catch (error) {
+      console.log('Error while fetching..', error)
+    }
+  }
+
+  function handleClick(id: string, title: string, author: string, description: string) {
+    setUpdate({
+      id,
+      title,
+      author,
+      description,
+    })
+  }
   return (
     <>
       <HomeContainer>
-        <InputForm />
-        <BookItemsList />
+        <InputForm setStateChange={setStateChange} update={update} />
+        <BookItemsList {...data} handleClick={handleClick} />
       </HomeContainer>
     </>
   )
