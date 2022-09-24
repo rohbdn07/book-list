f8be72d59c9b6ea4172277724fc3e2e7ee8c1972
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-09-24 13:22:58 +0300

    interface type added

--- client/src/components/buttons/ButtonGroup.tsx
+++ client/src/components/buttons/ButtonGroup.tsx
@@ -10,11 +10,17 @@ const Container = styled('div')({
   marginTop: '10px',
 })
 
+interface ButtonGroupProps {
+  createNewSubmit: (e: any) => void
+  updateSubmit: (e: any) => void
+  deleteSubmit: (e: any) => void
+}
+
 /**
  * @description it shows group of buttons
  * @returns JSX Element
  */
-const ButtonGroupEl = ({ createNewSubmit, updateSubmit, deleteSubmit }: any) => {
+const ButtonGroupEl = ({ createNewSubmit, updateSubmit, deleteSubmit }: ButtonGroupProps) => {
   return (
     <ButtonGroup aria-label='button group'>
       <Container>
